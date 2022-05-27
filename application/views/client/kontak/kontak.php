2d3ae8dbd76ad5e7d6635438bdfa7289f1351673
muhammadroyyan11 <royyanmz87@gmail.com>
2022-05-27 17:58:51 +0700

    tes lagi

--- application/views/client/kontak/kontak.php
+++ application/views/client/kontak/kontak.php
@@ -3,8 +3,8 @@
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
             <div class="col-md-9 ftco-animate pb-0 text-center">
-                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Properties <i class="fa fa-chevron-right"></i></span></p>
-                <h1 class="mb-3 bread">Perumahan</h1>
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Kontak <i class="fa fa-chevron-right"></i></span></p>
+                <h1 class="mb-3 bread">Kontak Kami</h1>
             </div>
         </div>
     </div>
