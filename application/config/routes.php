95a97cf2349cc871ae3ac0152a86b4d97d0466da
muhammadroyyan11 <royyanmz87@gmail.com>
2022-06-02 14:34:11 +0700

    tes

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,5 +54,5 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 
-$route['admin'] = 'admin/auth';
+$route['admin'] = 'admin/dashboard';
 $route['admin/login'] = 'admin/auth';
--- application/views/client/template.php
+++ application/views/client/template.php
@@ -153,60 +153,61 @@
     <script src="<?= base_url() ?>assets/client/js/google-map.js"></script>
     <script src="<?= base_url() ?>assets/client/js/main.js"></script>
 
-    <!--Start of Tawk.to Script-->
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/62909c62b0d10b6f3e745058/1g42e5ovo';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+    <?php if (is_user()) { ?>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {},
+                Tawk_LoadStart = new Date();
+            (function() {
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/62909c62b0d10b6f3e745058/1g42e5ovo';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
 
-    <script>
-        var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?1600';
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = url;
-        var options = {
-            "enabled": true,
-            "chatButtonSetting": {
-                "backgroundColor": "#4dc247",
-                "ctaText": "",
-                "borderRadius": "25",
-                "marginLeft": "0",
-                "marginBottom": "25",
-                "marginRight": "100",
-                "position": "right"
-            },
-            "brandSetting": {
-                "brandName": "SIKUPER",
-                "brandSubTitle": "Typically replies within a day",
-                "brandImg": "https://ibb.co/QjJj8Hb",
-                "welcomeText": "Hi,\nAda yang bisa saya bantu?",
-                "messageText": "Hello, Saya ingin mengetahui Informasi lebih lanjut terkait perumahan",
-                "backgroundColor": "#0a5f54",
-                "ctaText": "Mulai Chat",
-                "borderRadius": "100",
-                "autoShow": false,
-                "phoneNumber": "62852228282728"
-            }
-        };
-        s.onload = function() {
-            CreateWhatsappChatWidget(options);
-        };
-        var x = document.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-    </script>
+        <script>
+            var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?1600';
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = url;
+            var options = {
+                "enabled": true,
+                "chatButtonSetting": {
+                    "backgroundColor": "#4dc247",
+                    "ctaText": "",
+                    "borderRadius": "25",
+                    "marginLeft": "0",
+                    "marginBottom": "25",
+                    "marginRight": "100",
+                    "position": "right"
+                },
+                "brandSetting": {
+                    "brandName": "SIKUPER",
+                    "brandSubTitle": "Typically replies within a day",
+                    "brandImg": "https://ibb.co/QjJj8Hb",
+                    "welcomeText": "Hi,\nAda yang bisa saya bantu?",
+                    "messageText": "Hello, Saya ingin mengetahui Informasi lebih lanjut terkait perumahan",
+                    "backgroundColor": "#0a5f54",
+                    "ctaText": "Mulai Chat",
+                    "borderRadius": "100",
+                    "autoShow": false,
+                    "phoneNumber": "62852228282728"
+                }
+            };
+            s.onload = function() {
+                CreateWhatsappChatWidget(options);
+            };
+            var x = document.getElementsByTagName('script')[0];
+            x.parentNode.insertBefore(s, x);
+        </script>
 
+    <?php } ?>
 
 
     <!--End of Tawk.to Script-->
